aa77948ad951649444c75a92d0e7566eb3c9ef71
svan9 <ivan.selyakovich@gmail.com>
2023-08-05 17:36:32 +0300

    05.08.23:17.36

--- main.js
+++ main.js
@@ -1,138 +1,134 @@
 const ru = "абвгдеёжзийклмнопрстуфхцчшщэюяьъыАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯЫЬЪ";
 const en = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const dict = "./source/dictionary.json"; //"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20230705T171302Z.ccadeec9e136544a.76e97b76f5f7ebf3e60f63c8c7d1d11c5829898b&lang=ru-ru&text="
 
 function offFileName() {
-    if (document.location.href.match(new RegExp("index.html")) == null) return;
-    document.location.href = document.location.href.replace("index.html", "");
+	if (document.location.href.match(new RegExp("index.html")) == null) return;
+	document.location.href = document.location.href.replace("index.html", "");
 }
 
 function putWordRows() {
-    [...$(".wordRow")].forEach(
-        (e) => (e.innerHTML += `<input type="text" maxlength="1"></input>`.repeat(e.dataset.length))
-    );
-    $(".wordRow").on("click", "input", function (e) {
-        e.preventDefault();
-        if (e.delegateTarget.classList.has("disabled")) return;
-        this.setSelectionRange(0, this.value.length);
-    });
-    function off(e) {
-        e.preventDefault();
-    }
-    $(".wordRow > input").on("dblclick", off);
-    $(".wordRow > input").on("dragstart", off);
-    $(".wordRow > input").on("dragend", off);
-    $(".wordRow > input").on("drag", off);
-    $(".wordRow > input").on("drop", off);
-    $(".wordRow > input").on("mousemove", off);
+	[...$(".wordRow")].forEach(
+		(e) => (e.innerHTML += `<input type="text" maxlength="1"></input>`.repeat(e.dataset.length))
+	);
+	$(".wordRow").on("click", "input", function (e) {
+		e.preventDefault();
+		if (e.delegateTarget.classList.has("disabled")) return;
+		this.setSelectionRange(0, this.value.length);
+	});
+	function off(e) {
+		e.preventDefault();
+	}
+	$(".wordRow > input").on("dblclick", off);
+	$(".wordRow > input").on("dragstart", off);
+	$(".wordRow > input").on("dragend", off);
+	$(".wordRow > input").on("drag", off);
+	$(".wordRow > input").on("drop", off);
+	$(".wordRow > input").on("mousemove", off);
 
-    $(".wordRow").on("keydown", "input", function (e) {
-        e.preventDefault();
-        if (e.delegateTarget.classList.has("disabled")) return;
-        if (e.key == "Backspace") {
-            e.target.value = "";
-            new Promise((res, rej) => {
-                res(e.delegateTarget.children.item([...e.delegateTarget.children].indexOf(this) - 1));
-            })
-                .then((next) => {
-                    next.focus(), next.setSelectionRange(0, next.value.length);
-                    return next;
-                })
-                .catch((_) => {});
-        } else if (ru.indexOf(e.key) != -1) {
-            e.target.value = e.key;
-            new Promise((res, rej) => {
-                res(e.delegateTarget.children.item([...e.delegateTarget.children].indexOf(this) + 1));
-            })
-                .then((next) => {
-                    next.focus(), next.setSelectionRange(0, next.value.length);
-                    return next;
-                })
-                .catch((_) => {
-                    document.activeElement.blur();
-                });
-        }
-    });
+	$(".wordRow").on("keydown", "input", function (e) {
+		e.preventDefault();
+		if (e.delegateTarget.classList.has("disabled")) return;
+		if (e.key == "Backspace") {
+			e.target.value = "";
+			new Promise((res, rej) => {
+				res(e.delegateTarget.children.item([...e.delegateTarget.children].indexOf(this) - 1));
+			})
+				.then((next) => {
+					next.focus(), next.setSelectionRange(0, next.value.length);
+					return next;
+				})
+				.catch((_) => {});
+		} else if (ru.indexOf(e.key) != -1) {
+			e.target.value = e.key;
+			new Promise((res, rej) => {
+				res(e.delegateTarget.children.item([...e.delegateTarget.children].indexOf(this) + 1));
+			})
+				.then((next) => {
+					next.focus(), next.setSelectionRange(0, next.value.length);
+					return next;
+				})
+				.catch((_) => {
+					document.activeElement.blur();
+				});
+		}
+	});
 }
 
 function getWordByWordRow(row) {
-    return [...row.children].reduce((a, e) => a + e.value, "");
+	return [...row.children].reduce((a, e) => a + e.value, "");
 }
 
 async function getInfoWord(word) {
-    const link =
-        "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20230705T171302Z.ccadeec9e136544a.76e97b76f5f7ebf3e60f63c8c7d1d11c5829898b&lang=ru-ru&text=" +
-        decodeURI(word);
-    return await new Promise((res, rej) => {
-        fetch(link).then(async (e) => {
-            res(await e.json());
-        });
-    });
+	return await new Promise((res, rej) => {
+		fetch(dict).then(async (e) => {
+			res((await e.json()).wordlist.filter((e) => e.name == word).at(0).name);
+		});
+	});
 }
 
 async function wordIsNotExist(word) {
-    const link =
-        "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20230705T171302Z.ccadeec9e136544a.76e97b76f5f7ebf3e60f63c8c7d1d11c5829898b&lang=ru-ru&text=" +
-        decodeURI(word);
-    return await new Promise((res, rej) => {
-        fetch(link).then(async (e) => {
-            res((await e.json()).def.length != 0);
-        });
-    });
+	return await new Promise((res, rej) => {
+		fetch(dict).then(async (e) => {
+			console.log();
+			res((await e.json()).wordlist.filter((e) => e.name == word).length != 0);
+		});
+	});
 }
 
 function randrange(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min) - 0;
+	return Math.floor(Math.random() * (max - min + 1) + min) - 0;
 }
 
 function getRandomWord(length) {
-    return new Promise((res, rej) =>
-        $.ajax({
-            url: "./source/dictionary.json",
-            async: false,
-            dataType: "json",
-            success: function (data) {
-                function getLength() {
-                    let w = data.wordlist[randrange(0, data.wordlist.length - 1)];
-                    if (w.name.length != length) w = getLength();
-                    return w;
-                }
-                res(getLength().name.toLowerCase());
-            },
-        })
-    );
+	return new Promise((res, rej) =>
+		$.ajax({
+			url: "./source/dictionary.json",
+			async: false,
+			dataType: "json",
+			success: function (data) {
+				function getLength() {
+					let w = data.wordlist[randrange(0, data.wordlist.length - 1)];
+					if (w.name.length != length) w = getLength();
+					return w;
+				}
+				res(getLength().name.toLowerCase());
+			},
+		})
+	);
 }
 
 function pushHTML(name, where) {
-    let parent = $(document.createElement("div"));
-    parent.attr("from", name);
-    parent.load(`./html/${name}.html`);
-    $(where).append(parent);
+	let parent = $(document.createElement("div"));
+	parent.attr("from", name);
+	parent.load(`./html/${name}.html`);
+	$(where).append(parent);
 }
 
 window.onload = () => {
-    offFileName();
-    putWordRows();
+	offFileName();
+	putWordRows();
 };
 
 DOMTokenList.prototype.has = function (item) {
-    for (let i of this) if (i == item) return true;
-    return false;
+	for (let i of this) if (i == item) return true;
+	return false;
 };
 
 Array.prototype.has = function (item) {
-    if (this.find((e) => e == item) != undefined) return true;
-    return false;
+	if (this.find((e) => e == item) != undefined) return true;
+	return false;
 };
 
 window.addEventListener("load", (ev) => {
-    const randrange = (min, max) => parseInt(Math.random() * (max - min) + min);
+	const randrange = (min, max) => parseInt(Math.random() * (max - min) + min);
 
-    var sheet = new CSSStyleSheet();
+	var sheet = new CSSStyleSheet();
 
-    setInterval(() => {
-        sheet.replaceSync(
-            `::selection, -webkit-tap-highlight-color { background: ${`hsl(${randrange(0, 360)} 100% 50% / 20%);`}}`
-        );
-        document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
-    }, 200);
+	setInterval(() => {
+		sheet.replaceSync(
+			`::selection, -webkit-tap-highlight-color { background: ${`hsl(${randrange(0, 360)} 100% 50% / 20%);`}}`
+		);
+		document.adoptedStyleSheets = [...document.adoptedStyleSheets, sheet];
+	}, 200);
 });
--- src/game.js
+++ src/game.js
@@ -1,10 +1,10 @@
 function startGAME(urlParams) {
-    var finishWord = atob(urlParams.get("word"))
-        .split(",")
-        .map((e) => ru[e])
-        .join("");
+	var finishWord = atob(urlParams.get("word"))
+		.split(",")
+		.map((e) => ru[e])
+		.join("");
 
-    $("main").html(`
+	$("main").html(`
     <div class="wordsColumn" data-focus="0" data-complitedchars="" data-oplacechars="">
         <div class="wordRow" id="finishWord" data-length="5"></div>
             ${`<div class="wordRow disabled" data-length="5"></div>`.repeat(5)}
@@ -14,105 +14,121 @@ function startGAME(urlParams) {
     <div class="description">ввод букв осуществляется только на киррилице, при вводе с экранной клавиатуры телефона ввод букв осуществляется отдельно</div>
     <div class="message" id="wonMessage">победа</div>
     `);
-    var useWords = [];
-    var wordInfo = {};
+	var useWords = [];
+	var wordInfo = {};
 
-    $(".wordsColumn").on("mouseleave", "info", async function (e) {
-        var lbl = this.getElementsByTagName("label")[0];
-        lbl.innerHTML = "";
-    });
+	$("footer").on("click", ".char", async function () {
+		const key = this.innerHTML.toLowerCase();
+		if (ru.indexOf(key) != -1) {
+			try {
+				const local = $(".wordsColumn")[0].children.item($(".wordsColumn")[0].dataset.focus);
+				if (local.classList.has("disabled")) return;
+				const input = [...local.children].filter((c) => c.value.trim() == "");
+				input.at(0).focus();
+				input.at(0).value = key;
+				input.at(1).focus();
+			} catch (e) {
+				document.activeElement.blur();
+			}
+		}
+	});
 
-    $(".wordsColumn").on("mouseenter", "info", async function (e) {
-        var lbl = this.getElementsByTagName("label")[0];
-        if (wordInfo.def.length == 0) return;
-        lbl.innerHTML = wordInfo.def[0].tr
-            .map((e) => e.text)
-            .slice(0, 4)
-            .join(", ");
-    });
+	$(".wordsColumn").on("mouseleave", "info", async function (e) {
+		var lbl = this.getElementsByTagName("label")[0];
+		lbl.innerHTML = "";
+	});
 
-    $(".wordsColumn").on("mouseenter", ".wordRow", async function (e) {
-        const word = getWordByWordRow(this).toLowerCase();
-        if (!(word != "" && this.classList.has("disabled"))) return;
-        wordInfo = await getInfoWord(word);
-        var st = this.getBoundingClientRect();
-        this.style.cursor = "help";
+	$(".wordsColumn").on("mouseenter", "info", async function (e) {
+		var lbl = this.getElementsByTagName("label")[0];
+		if (wordInfo.def.length == 0) return;
+		lbl.innerHTML = wordInfo.def[0].tr
+			.map((e) => e.text)
+			.slice(0, 4)
+			.join(", ");
+	});
 
-        $(".wordsColumn info").css({
-            top: `${st.top + (st.height / 2 - $(".wordsColumn info")[0].getBoundingClientRect().height / 2)}px`,
-            left: `${st.left + st.width + 10}px`,
-        });
+	$(".wordsColumn").on("mouseenter", ".wordRow", async function (e) {
+		const word = getWordByWordRow(this).toLowerCase();
+		if (!(word != "" && this.classList.has("disabled"))) return;
+		wordInfo = await getInfoWord(word);
+		var st = this.getBoundingClientRect();
+		this.style.cursor = "help";
 
-        setTimeout(() => {
-            $(".wordsColumn info").css({
-                opacity: 1,
-            });
-        }, 500);
-    });
-    $(".wordsColumn").on("click", ".button#confirm", async function (e) {
-        if (this.classList.has("disabled")) return;
-        var parent = e.delegateTarget;
-        var local = parent.children.item(parent.dataset.focus);
-        if (parent.dataset.won == "true") {
-            local.classList.add("disabled");
-            return;
-        }
+		$(".wordsColumn info").css({
+			top: `${st.top + (st.height / 2 - $(".wordsColumn info")[0].getBoundingClientRect().height / 2)}px`,
+			left: `${st.left + st.width + 10}px`,
+		});
 
-        const word = getWordByWordRow(local).toLowerCase();
+		setTimeout(() => {
+			$(".wordsColumn info").css({
+				opacity: 1,
+			});
+		}, 500);
+	});
+	$(".wordsColumn").on("click", ".button#confirm", async function (e) {
+		if (this.classList.has("disabled")) return;
+		var parent = e.delegateTarget;
+		var local = parent.children.item(parent.dataset.focus);
+		if (parent.dataset.won == "true") {
+			local.classList.add("disabled");
+			return;
+		}
 
-        if (word.length != Number(local.dataset.length)) {
-            $("#wonMessage").html("слишком короткое слово").addClass("go").addClass("error");
-            setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
-            return;
-        }
-        if (!(await wordIsNotExist(word))) {
-            $("#wonMessage").html("слово не существует").addClass("go").addClass("error");
-            setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
-            return;
-        }
-        if (useWords.indexOf(word) != -1) {
-            $("#wonMessage").html("слово уже было использовано").addClass("go").addClass("error");
-            setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
-            return;
-        }
+		const word = getWordByWordRow(local).toLowerCase();
 
-        useWords.push(word);
+		if (word.length != Number(local.dataset.length)) {
+			$("#wonMessage").html("слишком короткое слово").addClass("go").addClass("error");
+			setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
+			return;
+		}
+		if (!(await wordIsNotExist(word))) {
+			$("#wonMessage").html("слово не существует").addClass("go").addClass("error");
+			setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
+			return;
+		}
+		if (useWords.indexOf(word) != -1) {
+			$("#wonMessage").html("слово уже было использовано").addClass("go").addClass("error");
+			setTimeout(() => $("#wonMessage").removeClass("go"), 5000);
+			return;
+		}
 
-        [...word].forEach((e, i) => {
-            setTimeout(() => {
-                if (e == finishWord.at(i)) {
-                    // true position
-                    local.children.item(i).classList.add("true_position_char");
-                    $("#rukb").find(`.char[name=${e}]`).addClass("true_position_char");
-                } else if (finishWord.indexOf(e) != -1) {
-                    // char is exists
-                    local.children.item(i).classList.add("is_exists_char");
-                    if ($("#rukb").find(`.char[name=${e}]`).hasClass("true_position_char")) return;
-                    $("#rukb").find(`.char[name=${e}]`).addClass("is_exists_char");
-                } else {
-                    // char is not exists
-                    local.children.item(i).classList.add("default_char");
-                    $("#rukb").find(`.char[name=${e}]`).addClass("default_char");
-                }
-            }, 100 * i);
-        });
-        let set = [...new Set([...word].map((e, i) => (e == finishWord.at(i) ? true : false)))];
+		useWords.push(word);
 
-        local.classList.add("disabled");
+		[...word].forEach((e, i) => {
+			setTimeout(() => {
+				if (e == finishWord.at(i)) {
+					// true position
+					local.children.item(i).classList.add("true_position_char");
+					$("#rukb").find(`.char[name=${e}]`).addClass("true_position_char");
+				} else if (finishWord.indexOf(e) != -1) {
+					// char is exists
+					local.children.item(i).classList.add("is_exists_char");
+					if ($("#rukb").find(`.char[name=${e}]`).hasClass("true_position_char")) return;
+					$("#rukb").find(`.char[name=${e}]`).addClass("is_exists_char");
+				} else {
+					// char is not exists
+					local.children.item(i).classList.add("default_char");
+					$("#rukb").find(`.char[name=${e}]`).addClass("default_char");
+				}
+			}, 100 * i);
+		});
+		let set = [...new Set([...word].map((e, i) => (e == finishWord.at(i) ? true : false)))];
 
-        if (set.length == 1 && set[0]) {
-            parent.dataset.won = "true";
-            $("#wonMessage").attr("class", "message won push").html("победа");
-            return;
-        }
+		local.classList.add("disabled");
 
-        parent.dataset.focus++;
+		if (set.length == 1 && set[0]) {
+			parent.dataset.won = "true";
+			$("#wonMessage").attr("class", "message won push").html("победа");
+			return;
+		}
 
-        parent.children.item(parent.dataset.focus).classList.remove("disabled");
-        if (parent.dataset.focus >= parent.children.length - 2) {
-            parent.dataset.won = "true";
-            $("#wonMessage").attr("class", "message error push").html("поражение");
-            this.classList.add("disabled");
-        }
-    });
+		parent.dataset.focus++;
+
+		parent.children.item(parent.dataset.focus).classList.remove("disabled");
+		if (parent.dataset.focus >= parent.children.length - 2) {
+			parent.dataset.won = "true";
+			$("#wonMessage").attr("class", "message error push").html("поражение");
+			this.classList.add("disabled");
+		}
+	});
 }
